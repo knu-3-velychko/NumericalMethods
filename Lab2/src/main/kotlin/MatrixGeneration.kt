b12349539bc034a5efe8a543a6a3a52880f9e2b8
Unknown <justataya@gmail.com>
2019-09-18 12:21:07 +0300

    Added method to generate  diagonal dominant matrix

--- Lab2/src/main/kotlin/MatrixGeneration.kt
+++ Lab2/src/main/kotlin/MatrixGeneration.kt
@@ -1,28 +1,31 @@
 import java.math.BigDecimal
+import kotlin.math.sign
 
-fun getRandomMatrix(size: Int, range: Int): Matrix {
-    val matrix = Array(size) { Array<BigDecimal>(size) { BigDecimal(0) } };
-    val max = BigDecimal(2 * range)
-    for (i in 1..size) {
-        for (j in 1..size) {
-            val randFromDouble = BigDecimal(Math.random())
-            val actualRandomDec = randFromDouble.multiply(max) - max / BigDecimal(2)
-            matrix[i][j] = actualRandomDec
-        }
+object MatrixGenerator {
+    fun getRandomMatrix(size: Int, range: Int): Matrix {
+        return Matrix(getRandomArray(size, BigDecimal(2 * range)))
     }
-    return Matrix(matrix)
-}
 
-//
-//fun getMatrix(size: Int, range: Int): Matrix {
-//    val matrix = Array(size) { Array<BigDecimal>(size) { BigDecimal(0) } };
-//    val max = BigDecimal(2 * range)
-//    for (i in 1..size) {
-//        for (j in 1..size) {
-//            val randFromDouble = BigDecimal(Math.random())
-//            val actualRandomDec = randFromDouble.multiply(max) - max / BigDecimal(2)
-//            matrix[i][j] = actualRandomDec
-//        }
-//    }
-//    return Matrix(matrix)
-//}
+
+    fun getDiagonallyDominantMatrix(size: Int, range: Int): Matrix {
+        val max = BigDecimal(2 * range)
+        var matrix = getRandomArray(size, max)
+        for (i in 0 until size - 1) {
+            val randFromDouble = BigDecimal(Math.random())
+            matrix[i][i] = matrix[i].fold(BigDecimal.ZERO) { acc, e -> acc + e.abs() }
+            matrix[i][i] = randFromDouble.multiply(max) - max / BigDecimal(2)
+            matrix[i][i] *= BigDecimal((-2..1).random().sign)
+        }
+        return Matrix(matrix)
+    }
+
+    private fun getRandomArray(size: Int, max: BigDecimal): Array<Array<BigDecimal>> {
+        val matrix = Array(size) {
+            Array(size) {
+                val randFromDouble = BigDecimal(Math.random())
+                randFromDouble.multiply(max) - max / BigDecimal(2)
+            }
+        }
+        return matrix
+    }
+}
