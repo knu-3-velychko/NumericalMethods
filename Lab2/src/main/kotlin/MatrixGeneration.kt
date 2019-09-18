c170de42aac4efab664b842e66b5923f9051e0f9
Unknown <justataya@gmail.com>
2019-09-18 12:52:10 +0300

    Added Hilbert matrix generation

--- Lab2/src/main/kotlin/MatrixGeneration.kt
+++ Lab2/src/main/kotlin/MatrixGeneration.kt
@@ -2,14 +2,12 @@ import java.math.BigDecimal
 import kotlin.math.sign
 
 object MatrixGenerator {
-    fun getRandomMatrix(size: Int, range: Int): Matrix {
-        return Matrix(getRandomArray(size, BigDecimal(2 * range)))
-    }
-
+    fun getRandomMatrix(size: Int, range: Int) =
+        Matrix(getRandomArray(size, BigDecimal(2 * range)))
 
     fun getDiagonallyDominantMatrix(size: Int, range: Int): Matrix {
         val max = BigDecimal(2 * range)
-        var matrix = getRandomArray(size, max)
+        val matrix = getRandomArray(size, max)
         for (i in 0 until size - 1) {
             val randFromDouble = BigDecimal(Math.random())
             matrix[i][i] = matrix[i].fold(BigDecimal.ZERO) { acc, e -> acc + e.abs() }
@@ -19,13 +17,19 @@ object MatrixGenerator {
         return Matrix(matrix)
     }
 
-    private fun getRandomArray(size: Int, max: BigDecimal): Array<Array<BigDecimal>> {
-        val matrix = Array(size) {
+    fun getHilbertMatrix(size: Int) =
+        Array(size) { i ->
+            Array(size) { j -> BigDecimal(1 / (i + j + 1)) }
+        }
+
+    private fun getRandomArray(size: Int, max: BigDecimal) =
+        Array(size) {
             Array(size) {
                 val randFromDouble = BigDecimal(Math.random())
                 randFromDouble.multiply(max) - max / BigDecimal(2)
             }
         }
-        return matrix
-    }
+
+    fun getVector(size: Int, matrix: Matrix, root: Array<BigDecimal>) =
+        Array(size){}
 }
