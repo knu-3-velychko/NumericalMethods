cb5f7b61927966512b47170939dd48debf9493a0
Unknown <justataya@gmail.com>
2019-09-19 21:39:05 +0300

    Added Jacobi method

--- Lab2/src/main/kotlin/JacobiMethod.kt
+++ Lab2/src/main/kotlin/JacobiMethod.kt
@@ -1,4 +1,25 @@
-class JacobiMethod {
+import kotlin.math.absoluteValue
 
-    
+class JacobiMethod(val matrix: Array<Array<Double>>, var x:Array<Double>, val size: Int, val e:Double) {
+    val result: Array<Double> by lazy {
+        var tmp = Array(size) { 0.0 }
+        var norm = e*10
+        while(norm>e){
+            for(i in 0 until size){
+                tmp[i]=matrix[i][size]
+                for(j in 0 until size){
+                    if(i!=j)
+                        tmp[i]-=matrix[i][j]*x[j]
+                }
+                tmp[i]/=matrix[i][i]
+            }
+            norm=(x[0]-tmp[0]).absoluteValue
+            for (i in 0 until size){
+                if((x[i]-tmp[i]).absoluteValue>norm)
+                    norm=(x[i]-tmp[i]).absoluteValue
+                x[i]=tmp[i]
+            }
+        }
+        x
+    }
 }
