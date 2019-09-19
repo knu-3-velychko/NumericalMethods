f77db22548d2921bc972fdb33c3a3fa62dbe0d61
Unknown <justataya@gmail.com>
2019-09-19 22:21:24 +0300

    Added Seidel method

--- Lab2/src/main/kotlin/SeidelMethod.kt
+++ Lab2/src/main/kotlin/SeidelMethod.kt
@@ -1,5 +1,27 @@
-class SeidelMethod(val matrix: Array<Array<Double>>, val vector: Array<Double>) {
-    val result: Array<Double> by lazy{
-        vector
+import kotlin.math.sqrt
+
+class SeidelMethod(val matrix: Array<Array<Double>>, var x: Array<Double>, val size: Int, val e: Double) {
+    val result: Array<Double> by lazy {
+        var norm = e * e * 10
+
+        var p = Array(size) { 0.0 }
+        while (sqrt(norm) > e) {
+            for (i in 0 until size) {
+                p[i] = x[i]
+            }
+            for (i in 0 until size) {
+                var tmp = 0.0
+                for (j in 0 until i)
+                    tmp += matrix[i][j] * x[j]
+                for (j in i + 1 until size)
+                    tmp += matrix[i][j] * p[j]
+                x[i] = (matrix[i][size] - tmp) / matrix[i][i]
+            }
+            norm = 0.0
+            for (i in 0 until size)
+                norm += (x[i] - p[i]) * (x[i] - p[i])
+        }
+        x
     }
+
 }
