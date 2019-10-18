ebf8b01eae3fc9f162a9e5806caa2fa7802e8f0c
Tai <justataya@gmail.com>
2019-10-18 18:41:51 +0300

    Progress commit

--- Lab4/src/main/kotlin/JacobiMethod.kt
+++ Lab4/src/main/kotlin/JacobiMethod.kt
@@ -7,15 +7,28 @@ class JacobiMethod(
 ) :
     Method {
     override val result: Array<Double> by lazy {
-        val M = matrix.getDiagonal()
-        val N = (-1.0 * (matrix - matrix.getDiagonal()))
+        val M = matrix.getDiagonalInvert()
+        val N = (matrix - matrix.getDiagonal())
 
-        do {
-
-        } while (true)
-
-
-        b
+//        for (i in 0 until size) {
+//            for (j in 0 until size)
+//                print("${N[i, j]} ")
+//            println()
+//        }
+//        println()
+        var norm = 1.0
+        var nextX = Array(matrix.size) { 0.0 }
+        while (norm > e) {
+            nextX = M * (b + (-1.0 * N)*x)
+            norm = 0.0
+            for (i in 0 until size)
+                norm += (x[i] - nextX[i]) * (x[i] - nextX[i])
+//            for (i in nextX)
+//                print("$i ")
+//            println()
+            x = nextX
+        }
+        x
     }
     override var iterations: Int? = null
 }
