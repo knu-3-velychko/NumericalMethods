e535d91c091bd7394826c756639983dc9d02509b
Unknown <justataya@gmail.com>
2019-10-29 12:05:31 +0200

    Initialized project

---
+++ Lab6/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+class Main {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("I luv ya vawy much <3 Vawy not like wavy plasters, like very OwO")
+        }
+    }
+
+}
