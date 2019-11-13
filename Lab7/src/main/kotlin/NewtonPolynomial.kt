713a547c1f2c5c6f0d9fc682cf06c0afb1215a97
Unknown <justataya@gmail.com>
2019-11-13 14:35:24 +0200

    Added Newton's Polynomial Interpolation

--- Lab7/src/main/kotlin/NewtonPolynomial.kt
+++ Lab7/src/main/kotlin/NewtonPolynomial.kt
@@ -1,23 +1,31 @@
-class NewtonPolynomial(val points: Array<Point>) : Method {
+class NewtonPolynomial(private val points: Array<Point>) : Method {
+    private val size = points.size
+    private val differenceTable = Array(size) { Array(size) { 0.0 } }
+
     override val result: DoubleArray by lazy {
         val size = points.size
         var res = DoubleArray(size) { 0.0 }
+        var factorial = 1.0
         var product = doubleArrayOf(1.0)
-
+        fillDifferenceTable()
 
         for (i in 0 until size) {
-            res += product * getDividedDifference(i, 0)
+            res += product * differenceTable[i][0] * (1 / factorial)
+            factorial *= i + 1
             product *= doubleArrayOf(1.0, -points[i].x)
         }
         res
     }
 
-    private fun getDividedDifference(pow: Int, index: Int): Double {
-        if (pow == 0)
-            return points[index].y
-        return (getDividedDifference(pow - 1, index + 1) -
-                getDividedDifference(pow - 1, index)) /
-                (points[index + 1].x - points[index].x)
+    private fun fillDifferenceTable() {
+        for (i in 0 until size) {
+            differenceTable[0][i] = points[i].y
+        }
+        for (i in 1 until size) {
+            for (j in 0 until size - i)
+                differenceTable[i][j] =
+                    (differenceTable[i - 1][j + 1] - differenceTable[i - 1][j]) / (points[j + 1].x - points[j].x)
+        }
     }
 
     operator fun DoubleArray.plus(array: DoubleArray): DoubleArray {
@@ -33,5 +41,9 @@ class NewtonPolynomial(val points: Array<Point>) : Method {
             result[size - i] += array[size2 - i]
         }
         return result
+    }
+
+    private fun getProduct() {
+
     }
 }
